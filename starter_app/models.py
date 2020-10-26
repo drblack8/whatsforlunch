aecc5af63dfe0f0a7a5a408444b3ff6194223199
Dan Black <danielb0318@gmail.com>
2020-10-26 16:36:38 -0400

    First draft for Users, Comments & Posts

--- starter_app/models.py
+++ starter_app/models.py
@@ -4,39 +4,58 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
 
+
 class User(db.Model, UserMixin):
-  __tablename__ = 'users'
+    __tablename__ = 'users'
 
-  id = db.Column(db.Integer, primary_key = True)
-  username = db.Column(db.String(40), nullable = False, unique = True)
-  email = db.Column(db.String(255), nullable = False, unique = True)
-  hashed_password = db.Column(db.String(255), nullable = False)
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(40), nullable=False, unique=True)
+    email = db.Column(db.String(255), nullable=False, unique=True)
+    hashed_password = db.Column(db.String(255), nullable=False)
+    bio = db.Column(db.String(255))
+    pfp = db.Column(db.String(255))
 
-  posts = db.relationship("User", back_populates="users")
+    posts = db.relationship("Post", back_populates="users")
+    comments = db.relationship("Comment", back_populates="users")
 
-  @property
-  def password(self):
-      return self.hashed_password
+    @property
+    def password(self):
+        return self.hashed_password
 
-  @password.setter
-  def password(self, password):
-      self.hashed_password = generate_password_hash(password)
+    @password.setter
+    def password(self, password):
+        self.hashed_password = generate_password_hash(password)
 
-  def check_password(self, password):
-      return check_password_hash(self.password, password)
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
 
-  def to_dict(self):
-    return {
-      "id": self.id,
-      "username": self.username,
-      "email": self.email
-    }
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "username": self.username,
+            "email": self.email
+        }
+
 
 class Post(db.Model):
-  __tablename__ = 'posts'
+    __tablename__ = 'posts'
 
-  id = db.Column(db.Integer, primary_key = True)
-  image_url = db.Column(db.String(40), nullable = False, unique = True)
-  user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    id = db.Column(db.Integer, primary_key=True)
+    image_url = db.Column(db.String(40), nullable=False, unique=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    desc = db.Column(db.String(255))
 
-  users = db.relationship("User", back_populates="posts")
+    users = db.relationship("User", back_populates="posts")
+    comments = db.relationship("Comment", back_populates="posts")
+
+
+class Comment(db.Model):
+    __tablename__ = 'comments'
+
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(255), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    post_id = db.Column(db.Integer, db.ForeignKey("posts.id"))
+
+    posts = db.relationship("Post", back_populates="comments")
+    users = db.relationship("User", back_populates="comments")
