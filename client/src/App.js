1456aeb9e5ddba24c9d83e7aa78b4cee94d070c3
Jaron Degen <jaron.degen90@gmail.com>
2020-10-27 01:29:17 +0100

    fix merge conflicts

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,6 @@ import Profile from './pages/Profile'
 import Feed from './pages/Feed'
 import UserList from './components/UsersList';
 import NavBar from './components/nav/NavBar'
-import UserList from './components/UsersList';
 import UploadPage from './components/PhotoUpload/UploadPage';
 
 function App() {
@@ -12,25 +11,22 @@ function App() {
   return (
     <BrowserRouter>
         <NavBar />
-        {/* <nav>
+        <nav>
             <ul>
                 <li><NavLink to="/" activeclass="active">Home</NavLink></li>
                 <li><NavLink to="/users" activeclass="active">Users</NavLink></li>
                 <li><NavLink to="/profile" activeclass="active">Profile</NavLink></li>
                 <li><NavLink to="/posts/new" activeclass="active">Post</NavLink></li>
             </ul>
-        </nav> */}
+        </nav> 
         <Switch>
             <Route path="/users" component={UserList} />
             <Route path="/profile" component={Profile} />
-            <Route path="/" component={Feed} />
             <Route path="/users">
                 <UserList />
             </Route>
             <Route path="/posts/new" component={UploadPage} />
-            <Route path="/">
-                <h1>My Home Page</h1>
-            </Route>
+            <Route path="/" component={Feed} />
         </Switch>
     </BrowserRouter>
   );
