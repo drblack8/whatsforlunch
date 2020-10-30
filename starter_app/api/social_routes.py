4c39fe2ce6ca9aceb3d70d82359502b6d321b2bc
Dan Black <danielb0318@gmail.com>
2020-10-30 16:46:07 -0400

    first draft with new social routeq

--- starter_app/api/social_routes.py
+++ starter_app/api/social_routes.py
@@ -7,11 +7,15 @@ social_routes = Blueprint('social', __name__)
 
 @social_routes.route('/', methods=['POST'])
 def new_follow():
-    e
     data = request.get_json()
-    new_social = Social(
-        user=data["user_id"],
-        following=data["follow_id"])
-    db.session.add(new_social)
-    db.session.commit()
-    return data['user_id']
+    check = Social.query.filter_by(
+        user=data["user_id"], following=data["follow_id"])
+    if exist is None:
+        new_social = Social(
+            user=data["user_id"],
+            following=data["follow_id"])
+        db.session.add(new_social)
+        db.session.commit()
+        return data['user_id']
+    else:
+        return False
