87da11619de41f3db3ca57dc2ea3313f95e9c695
Jaron Degen <jaron.degen90@gmail.com>
2020-12-03 16:05:54 +0100

    fix image upload quality

--- client/src/components/PhotoUpload/PhotoUpload.js
+++ client/src/components/PhotoUpload/PhotoUpload.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState, useContext } from 'react';
 import { uploadFile } from 'react-s3';
 import  { configuration } from '../../config/index.js'
-import { useDispatch } from 'react-redux';
 import '../../style/post-page.css'
 import Button from '@material-ui/core/Button';
 import { TextField } from '@material-ui/core';
@@ -20,13 +19,10 @@ const config = {
 }
 
 const PhotoUpload = () => {
-    const dispatch = useDispatch();
     const [caption, setCaption] = useState('')
-    const [ready, setReady] = useState(true)
+    const [ready, setReady] = useState(false)
     const [selectedFile, setSelectedFile] = useState()
-    const [preview, setPreview] = useState()
     const [crop, setCrop] = useState({unit: "%", width: 50, height: 50, x: 25, y: 25, aspect: 1 / 1})
-    const [croppedImageUrl, setCroppedImageUrl] = useState(null)
     const [imageRef, setImageRef] = useState()
     const [croppedImage, setCroppedImage] = useState()
     const [src, setSrc] = useState()
@@ -36,7 +32,9 @@ const PhotoUpload = () => {
     //crop magic
     // https://levelup.gitconnected.com/crop-images-on-upload-in-your-react-app-with-react-image-crop-5f3cd0ad2b35
     const onImageLoaded = image => {
+        setCrop(crop);
         setImageRef(image)
+        getCroppedImg(image, crop)
     }
 
     const onCropChange = (crop) => {
@@ -45,7 +43,7 @@ const PhotoUpload = () => {
 
     const onCropComplete = crop => {
         if (imageRef && crop.width && crop.height) {
-            setCroppedImageUrl(getCroppedImg(imageRef, crop))
+            getCroppedImg(imageRef, crop)
         }
     }
 
@@ -53,22 +51,38 @@ const PhotoUpload = () => {
         const canvas = document.createElement("canvas");
         const scaleX = image.naturalWidth / image.width;
         const scaleY = image.naturalHeight / image.height;
-        canvas.width = crop.width;
-        canvas.height = crop.height;
+        var originWidth = crop.width * scaleX;
+        var originHeight = crop.height * scaleY;
+        // maximum width/height
+        var maxWidth = 1200, maxHeight = 1200 / (16 / 9);
+        var targetWidth = originWidth,
+          targetHeight = originHeight;
+        if (originWidth > maxWidth || originHeight > maxHeight) {
+          if (originWidth / originHeight > maxWidth / maxHeight) {
+            targetWidth = maxWidth;
+            targetHeight = Math.round(maxWidth * (originHeight / originWidth));
+          } else {
+            targetHeight = maxHeight;
+            targetWidth = Math.round(maxHeight * (originWidth / originHeight));
+          }
+        }
+        // set canvas size
+        canvas.width = targetWidth;
+        canvas.height = targetHeight;
         const ctx = canvas.getContext("2d");
-        
-        ctx.drawImage(
-            image,
-            crop.x * scaleX,
-            crop.y * scaleY,
-            crop.width * scaleX,
-            crop.height * scaleY,
-            0,
-            0,
-            crop.width,
-            crop.height
-         )
     
+        ctx.drawImage(
+          image, 
+          crop.x * scaleX, 
+          crop.y * scaleY, 
+          crop.width * scaleX, 
+          crop.height * scaleY, 
+          0, 
+          0, 
+          targetWidth, 
+          targetHeight 
+        );
+            
         const reader = new FileReader()
         canvas.toBlob(blob => {
             reader.readAsDataURL(blob)
@@ -94,11 +108,9 @@ const PhotoUpload = () => {
 
     useEffect(() => {
         if (!selectedFile) {
-            setPreview(undefined)
             return
         }
         const objectUrl = URL.createObjectURL(selectedFile)
-        setPreview(objectUrl)
         return () => URL.revokeObjectURL(objectUrl)
     }, [selectedFile])
 
@@ -126,7 +138,7 @@ const PhotoUpload = () => {
                 body: JSON.stringify({ image_url:data.location, user_id:currentUserId, desc:caption }),
             })
             if (post.ok){
-                setReady(true)
+                setReady(false)
                 setCaption('')
                 wheelDiv.setAttribute("class", "loading-wheel-container hidden")
             }
@@ -138,7 +150,7 @@ const PhotoUpload = () => {
             setSelectedFile(undefined)
             return
         }
-        setReady(false)
+        setReady(true)
         setSelectedFile(e.target.files[0])
         const fileReader = new FileReader()
         fileReader.onloadend = () => {
@@ -154,7 +166,7 @@ const PhotoUpload = () => {
                 <img src={wheel}/>
             </div>
             <div className="upload-form">
-                {ready ? (
+                {!ready ? (
                 <div className="upload-button">
                 <input
                     accept="image/*"
@@ -185,7 +197,7 @@ const PhotoUpload = () => {
                     <TextField multiline={true} onChange={handleCaption} value={caption} placeholder="Write a caption"></TextField>
                 </div>
                 <div>
-                    <Button variant="contained" color="primary" id="upload-button" onClick={upload} disabled={ready}>
+                    <Button variant="contained" color="primary" id="upload-button" onClick={upload} disabled={!ready}>
                         Post
                     </Button>
                 </div>
