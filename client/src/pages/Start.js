dffbc8dbf96ba0b55f9f44002efc908eaf307723
Andrea <aganesh0988@gmail.com>
2020-12-08 11:49:28 -0600

    added angellist to start.js

--- client/src/pages/Start.js
+++ client/src/pages/Start.js
@@ -5,78 +5,81 @@ import '../style/start.css'
 import logo from '../style/images/WFL.jpg'
 
 const names = [
-    {
-      name: "Jaron Degen",
-      github: "https://github.com/jarondegen",
-      linkedin: "https://www.linkedin.com/in/jarondegen/"
-    },
-    {
-      name: "Daniel Black",
-      github: "https://github.com/drblack8",
-      linkedin: "https://www.linkedin.com/in/danielrobertblack/"
-    },
-    {
-      name: "Andrea Jackson",
-      github: "https://github.com/aganesh0988",
-      linkedin: "https://www.linkedin.com/in/andrea-jackson1/"
-    },
-    {
-      name: "Quincy Jones",
-      github: "https://github.com/doncibo",
-      linkedin: "https://www.linkedin.com/in/don-quincy-jones/"
-    }
-  ];
-  // ReactDOM.render(
-  //   <React.StrictMode>
-  //     <Provider store={store}>
-  //       <App />
-  //     </Provider>
-  //   </React.StrictMode>,
-  //   document.getElementById('root')
-  // );
-  const Footer = () => (
-    <footer className="footer">
-      <div className="footer-content">
-        {names.map((el, idx) => (
-          <div key ={idx} className={`name-div ${idx}`}>
-            <div>{el.name}</div>
-            <a className="icons" target="_blank" href={el.github}>
-              <i className="fab fa-2x fa-github-square"></i>
-            </a>
-            <a className="icons" target="_blank" href={el.linkedin}>
-              <i className="fab fa-2x fa-linkedin"></i>
-            </a>
-          </div>
-        ))}
-      </div>
-    </footer>
-  );
+  {
+    name: "Jaron Degen",
+    github: "https://github.com/jarondegen",
+    linkedin: "https://www.linkedin.com/in/jarondegen/"
+  },
+  {
+    name: "Daniel Black",
+    github: "https://github.com/drblack8",
+    linkedin: "https://www.linkedin.com/in/danielrobertblack/"
+  },
+  {
+    name: "Andrea Jackson",
+    github: "https://github.com/aganesh0988",
+    linkedin: "https://www.linkedin.com/in/andrea-jackson1/"
+  },
+  {
+    name: "Quincy Jones",
+    github: "https://github.com/doncibo",
+    linkedin: "https://www.linkedin.com/in/don-quincy-jones/"
+  }
+];
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <Provider store={store}>
+//       <App />
+//     </Provider>
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+const Footer = () => (
+  <footer className="footer">
+    <div className="footer-content">
+      {names.map((el, idx) => (
+        <div key={idx} className={`name-div ${idx}`}>
+          <div>{el.name}</div>
+          <a className="icons" target="_blank" href={el.github}>
+            <i className="fab fa-2x fa-github-square"></i>
+          </a>
+          <a className="icons" target="_blank" href={el.linkedin}>
+            <i className="fab fa-2x fa-linkedin"></i>
+          </a>
+          <a className="icons" target="_blank" href={el.angellist}>
+            <i className="fab fa-2x fa-angellist"></i>
+          </a>
+        </div>
+      ))}
+    </div>
+  </footer>
+);
 
 
 
 const Start = () => {
-    const [flip, setFlip] = useState(false)
+  const [flip, setFlip] = useState(false)
 
 
-    return (
-        <>
-            <div className="start-page-container">
-                <div className="start-logo-container">
-                    <img className="start-logo" src={logo} />
-                </div>
-                <div className="forms-container">
-                    {
-                        flip ? <SignUpForm />
-                            : <LoginForm />
-                    }
-                    <a className="log-or-sign" onClick={() => flip ? setFlip(false) : setFlip(true)}>
-                        {flip ? 'Already have an account?' : 'Don\'t have an account?'}
-                    </a>
-                </div>
-            </div>
-            <Footer />
-        </>
-    );
+  return (
+    <>
+      <div className="start-page-container">
+        <div className="start-logo-container">
+          <img className="start-logo" src={logo} />
+        </div>
+        <div className="forms-container">
+          {
+            flip ? <SignUpForm />
+              : <LoginForm />
+          }
+          <a className="log-or-sign" onClick={() => flip ? setFlip(false) : setFlip(true)}>
+            {flip ? 'Already have an account?' : 'Don\'t have an account?'}
+          </a>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
 }
 
 export default Start
